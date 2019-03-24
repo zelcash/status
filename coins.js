20875d9163251f3a239f9771a699fe093e2edfff
trial123Zel <38964930+trial123Zel@users.noreply.github.com>
2019-03-24 11:22:56 -0700

    add assets
    XMR, BCH, BTCZ, RVN, BTX, BTG, XSG, ZEN, SAFE, KMD, XZC, BTCP, USDT, ZER, ZCL, ANON, HUSH, BNB, ZRX

--- coins.js
+++ coins.js
@@ -55,6 +55,215 @@ var data =
             "discord" : null,
             "telegram" : null,
             "info" : "Zcash is a digital currency with strong privacy features. Transact efficiently and safely, with low fees, while ensuring digital transactions remain private."
+        },
+        {
+            "coin" : "BitcoinZ",
+            "ticker" : "btcz",
+            "logo" : "logos/BitcoinZ.svg",
+            "explorer" : "https://explorer.btcz.zelcore.io",
+            "website" : "https://btcz.rocks/en/",
+            "twitter" : "https://twitter.com/BitcoinZTeam",
+            "discord" : "https://discordapp.com/invite/u3dkbFs",
+            "telegram" : "https://t.me/joinchat/CDzlaRGMvBm4P2Z76sNclQ",
+            "info" : ""
+        },
+        {
+            "coin" : "Monero",
+            "ticker" : "XMR",
+            "logo" : "logos/Monero.svg",
+            "explorer" : "https://explorer.xmr.zelcore.io",
+            "website" : "https://ww.getmonero.org/",
+            "twitter" : null,
+            "discord" : null,
+            "telegram" : "https://telegram.me/bitmonero",
+            "info" : ""
+        },
+        {
+            "coin" : "Bitcoin Cash",
+            "ticker" : "BCH",
+            "logo" : "logos/BitcoinCash.svg",
+            "explorer" : "https://blockdozer.com/",
+            "website" : "https://www.bitcoincash.org/",
+            "twitter" : null,
+            "discord" : null,
+            "telegram" : "https://t.me/BitcoinCashUpdates",
+            "info" : ""
+        },
+        {
+            "coin" : "Ravencoin",
+            "ticker" : "RVN",
+            "logo" : "logos/Ravencoin.svg",
+            "explorer" : "https://explorer.rvn.zelcore.io/",
+            "website" : "https://ravencoin.org/",
+            "twitter" : "https://twitter.com/Ravencoin",
+            "discord" : "https://discordapp.com/invite/jn6uhur",
+            "telegram" : "https://t.me/RavencoinDev",
+            "info" : ""
+        },
+        {
+            "coin" : "Bitcore",
+            "ticker" : "BTX",
+            "logo" : "logos/Bitcore.svg",
+            "explorer" : "https://explorer.btx.zelcore.io/",
+            "website" : "http://bitcore.cc/",
+            "twitter" : "https://twitter.com/Bitcore_BTX",
+            "discord" : "https://discord.gg/9chHgCY",
+            "telegram" : "https://t.me/bitcore_cc",
+            "info" : ""
+        },
+        {
+            "coin" : "Bitcoin Gold",
+            "ticker" : "BTG",
+            "logo" : "logos/BitcoinGold.svg",
+            "explorer" : "https://explorer.bitcoingold.org/insight/",
+            "website" : "https://bitcoingold.org/",
+            "twitter" : "https://twitter.com/bitcoingold",
+            "discord" : "https://discord.gg/HmVUU6S",
+            "telegram" : "https://t.me/BitcoinGoldHQ",
+            "info" : ""
+        },
+        {
+            "coin" : "SnowGem",
+            "ticker" : "XSG",
+            "logo" : "logos/Snowgem.svg",
+            "explorer" : "https://explorer.xsg.zelcore.io/",
+            "website" : "https://snowgem.org/",
+            "twitter" : "https://twitter.com/snowgemofficial",
+            "discord" : "https://discord.gg/7a7XRZr",
+            "telegram" : "https://t.me/snowgemofficial",
+            "info" : ""
+        },
+        {
+            "coin" : "Horizen",
+            "ticker" : "ZEN",
+            "logo" : "logos/Zen.svg",
+            "explorer" : "https://explorer.zensystem.io/",
+            "website" : "https://horizen.global/",
+            "twitter" : "https://twitter.com/horizenglobal",
+            "discord" : "https://horizen.global/invite/discord",
+            "telegram" : "https://horizen.global/invite/telegram",
+            "info" : ""
+        },
+        {
+            "coin" : "SafeCoin",
+            "ticker" : "SAFE",
+            "logo" : "logos/Safecoin.svg",
+            "explorer" : "https://explorer.safe.zelcore.io/",
+            "website" : "https://www.safecoin.org/",
+            "twitter" : "https://twitter.com/safecoins",
+            "discord" : "https://discord.gg/vQgYGJz",
+            "telegram" : "https://t.me/joinchat/HCqlLxEEFR_SQPd9VjSpBw",
+            "info" : ""
+        },
+        {
+            "coin" : "Komodo",
+            "ticker" : "KMD",
+            "logo" : "logos/Komodo.svg",
+            "explorer" : "https://explorer.kmd.zelcore.io/",
+            "website" : "https://komodoplatform.com/",
+            "twitter" : "https://twitter.com/komodoplatform",
+            "discord" : "https://discordapp.com/invite/QpYmzqk",
+            "telegram" : "https://t.me/KomodoPlatform_Official",
+            "info" : ""
+        },
+        {
+            "coin" : "ZCoin",
+            "ticker" : "XZC",
+            "logo" : "logos/Zcoin.svg",
+            "explorer" : "https://explorer.zcoin.zelcore.io/",
+            "website" : "https://zcoin.io/",
+            "twitter" : "https://twitter.com/zcoinofficial",
+            "discord" : "https://discordapp.com/invite/4FjnQ2q",
+            "telegram" : "https://telegram.me/zcoinproject",
+            "info" : ""
+        },
+        {
+            "coin" : "Bitcoin Private",
+            "ticker" : "BTCP",
+            "logo" : "logos/BTCP.svg",
+            "explorer" : "https://explorer.btcprivate.org/",
+            "website" : "https://btcprivate.org/",
+            "twitter" : "https://twitter.com/bitcoinprivate",
+            "discord" : "https://discord.gg/46U2p4m",
+            "telegram" : "https://t.me/BTCPrivate",
+            "info" : ""
+        },
+        {
+            "coin" : "Tether",
+            "ticker" : "USDT",
+            "logo" : "logos/Tether.svg",
+            "explorer" : "https://omniexplorer.info/",
+            "website" : "https://tether.to/",
+            "twitter" : "https://twitter.com/Tether_to/",
+            "discord" : null,
+            "telegram" : null,
+            "info" : ""
+        },
+        {
+            "coin" : "Zero",
+            "ticker" : "ZER",
+            "logo" : "logos/Zero.svg",
+            "explorer" : "https://explorer.zer.zelcore.io/",
+            "website" : "https://zerocurrency.io/",
+            "twitter" : "https://twitter.com/ZeroCurrencies",
+            "discord" : null,
+            "telegram" : "https://t.me/zerocurrency",
+            "info" : ""
+        },
+        {
+            "coin" : "ZClassic",
+            "ticker" : "ZCL",
+            "logo" : "logos/Zclassic.svg",
+            "explorer" : "https://explorer.zcl.zelcore.io/",
+            "website" : "http://zclassic.org/",
+            "twitter" : "https://twitter.com/ZclassicCoin",
+            "discord" : "https://discord.gg/PmVX5jW",
+            "telegram" : null,
+            "info" : ""
+        },
+        {
+            "coin" : "ANON",
+            "ticker" : "ANON",
+            "logo" : "logos/ANON.svg",
+            "explorer" : "https://explorer.anon.zelcore.io/",
+            "website" : "https://www.anonfork.io/",
+            "twitter" : "https://twitter.com/ANON_WeAreANON",
+            "discord" : "https://discord.gg/9XQMspU",
+            "telegram" : "https://t.me/ANONFork",
+            "info" : ""
+        },
+        {
+            "coin" : "Hush",
+            "ticker" : "Hush",
+            "logo" : "logos/Hush.svg",
+            "explorer" : "https://explorer.hush.zelcore.io/",
+            "website" : "https://myhush.org/",
+            "twitter" : "https://twitter.com/MyHushTeam",
+            "discord" : "https://myhush.org/discord/index.html",
+            "telegram" : null,
+            "info" : ""
+        },
+        {
+            "coin" : "0x",
+            "ticker" : "ZRX",
+            "logo" : "logos/Zrx.svg",
+            "explorer" : "https://etherscan.io/address/0xe41d2489571d322189246dafa5ebde1f4699f498",
+            "website" : "https://0x.org/",
+            "twitter" : "https://twitter.com/0xproject",
+            "discord" : "https://discord.gg/d3FTX3M",
+            "telegram" : null,
+            "info" : ""
+        },
+        {
+            "coin" : "Binance Token",
+            "ticker" : "BNB",
+            "logo" : "logos/Binance.svg",
+            "explorer" : "https://etherscan.io/address/0xb8c77482e45f1f44de1745f52c74426c631bdd52",
+            "website" : "https://www.binance.com/",
+            "twitter" : "https://twitter.com/binance",
+            "discord" : null,
+            "telegram" : "https://www.binance.com/contactUsCn.html#telegrams",
+            "info" : ""
         }
     ]
 }
